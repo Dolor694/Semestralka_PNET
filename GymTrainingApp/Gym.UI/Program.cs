155b64af4c0982328a03dae41243f3ebd3490817
Dolor694 <lukasproisl@seznam.cz>
2026-04-01 15:01:06 +0200

    initial commit, basic structure

---
+++ GymTrainingApp/Gym.UI/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Gym - A simple application for managing gym trainings and plans for users and allows to automaticaly generate training plans.
+ * 
+ * @author: Lukáš Proisl
+ * @date: 2026-04
+ */
+
+
+
+/*
+ * Main entry point for the Gym application.
+ */
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
