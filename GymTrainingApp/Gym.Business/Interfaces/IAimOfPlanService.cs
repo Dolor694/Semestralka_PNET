4cc54d6dd83f775cc30c58cbed50548c4a7eecaa
Dolor694 <lukasproisl@seznam.cz>
2026-04-07 09:11:27 +0200

    AimOfPlanService implemented

---
+++ GymTrainingApp/Gym.Business/Interfaces/IAimOfPlanService.cs
@@ -0,0 +1,58 @@
+﻿using Gym.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gym.Business.Interfaces
+{
+    /*
+     * This interface defines the contract for the AimOfPlan service, 
+     * which is responsible for managing AimOfPlan entities in the application.
+     */
+    public interface IAimOfPlanService
+    {
+        /*
+         * This method retrieves an AimOfPlan by its ID.
+         * 
+         * @param id The ID of the AimOfPlan to retrieve.
+         * @return An AimOfPlanDTO representing the AimOfPlan with the specified ID, or null if not found.
+         */
+        AimOfPlanDTO? GetAimOfPlanById(int id);
+
+        /*
+        * This method creates a new AimOfPlan with the specified ID and name.
+        * 
+        * @param id The unique identifier for the new AimOfPlan.
+        * @param name The name of the new AimOfPlan.
+        * @return The created AimOfPlan object.
+        */
+        AimOfPlan CreateAimOfPlan(int id, string name);
+        
+        /*
+         * This method updates an existing AimOfPlan with the specified ID and new name.
+         * 
+         * @param id The unique identifier of the AimOfPlan to update.
+         * @param name The new name for the AimOfPlan. If null, the name will not be updated.
+         * @return The updated AimOfPlan object, or null if the AimOfPlan with the specified ID does not exist.
+         */
+        AimOfPlan UpdateAimOfPlan(int id, string? name);
+        
+        /*
+         * This method deletes an AimOfPlan with the specified ID.
+         * 
+         * @param id The unique identifier of the AimOfPlan to delete.
+         * @return true if the AimOfPlan was successfully deleted, or false if the AimOfPlan with the specified ID does not exist.
+         */
+        bool DeleteAimOfPlan(int id);
+
+        /*
+         * This method maps an AimOfPlan entity to an AimOfPlanDTO.
+         * 
+         * @param aimOfPlan The AimOfPlan entity to map.
+         * @return An AimOfPlanDTO representing the mapped AimOfPlan.
+         */
+        AimOfPlanDTO MapAimOfPlanToDTO(AimOfPlan aimOfPlan);
+    }
+}
---
+++ GymTrainingApp/Gym.Business/Services/AimOfPlanService.cs
@@ -0,0 +1,75 @@
+﻿using Gym.Business.Interfaces;
+using Gym.Models.Entities;
+using Gym.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gym.Business.Services
+{
+    public class AimOfPlanService : IAimOfPlanService
+    {
+        protected readonly IAimOfPlanRepository _aimOfPlanRepository;
+
+        public AimOfPlanService(IAimOfPlanRepository aimOfPlanRepository)
+        {
+            _aimOfPlanRepository = aimOfPlanRepository;
+        }
+
+        public AimOfPlan CreateAimOfPlan(int id, string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException("Name cannot be null or empty.", nameof(name));
+            }
+
+            AimOfPlan aim = new AimOfPlan{Id = id, Description = name};
+
+            _aimOfPlanRepository.Add(aim);
+
+            return aim;
+        }
+
+        public bool DeleteAimOfPlan(int id)
+        {
+            AimOfPlan? aim = _aimOfPlanRepository.GetById(id);
+
+            if (aim == null)
+            {
+                return false;
+            }
+
+            _aimOfPlanRepository.Delete(aim);
+
+            return true;
+        }
+
+        public AimOfPlanDTO? GetAimOfPlanById(int id)
+        {
+            AimOfPlan? aim = _aimOfPlanRepository.GetById(id);
+
+            if (aim == null)
+            {
+                return null;
+            }
+
+            return MapAimOfPlanToDTO(aim);
+        }
+
+        public AimOfPlanDTO MapAimOfPlanToDTO(AimOfPlan aimOfPlan)
+        {
+            return new AimOfPlanDTO(aimOfPlan.Id, aimOfPlan.Description);
+        }
+
+        public AimOfPlan UpdateAimOfPlan(int id, string? name)
+        {
+            AimOfPlan aim = new AimOfPlan { Id = id, Description = name ?? string.Empty };
+
+            _aimOfPlanRepository.Update(aim);
+
+            return aim;
+        }
+    }
+}
